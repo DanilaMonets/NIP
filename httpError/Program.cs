97cbdb7212616df8aa9b4b3f43fe03511d97dd56
Volodymyr Lysyshyn <lysyshynvova@gmail.com>
2018-09-13 17:48:55 +0100

    fix

---
+++ httpError/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace httpError
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
