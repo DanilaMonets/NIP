25ac486b40242cb1d846a6ef4ddf6cce3458cdfb
VasylSalabay <turboguy@ukr.net>
2018-09-15 23:23:16 +0300

    Fixed #2
    Added class HTTPErrorCollection

---
+++ httpError/HTTPErrorsCollection.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace httpError
+{
+    class HTTPErrorsCollection
+    {
+        private List<HTTPError> httpErrors;
+         
+        public HTTPErrorsCollection()
+        {
+            httpErrors = new List<HTTPError>();
+        }
+
+        public List<HTTPError> HttpErrors
+        {
+            get
+            {
+                return this.httpErrors;
+            }
+        }
+
+        public HTTPError this[int index]
+        {
+            get
+            {
+                return this.httpErrors[index];
+            }
+        }
+
+        public void Add(HTTPError error)
+        {
+            if(!this.httpErrors.Contains(error))
+            {
+                this.httpErrors.Add(error);
+            }
+        }
+
+        public bool Remove(HTTPError error)
+        { 
+            if(this.httpErrors.Contains(error))
+            {
+                return this.httpErrors.Remove(error);
+            }
+            return false;
+        }
+
+        public bool Contains(HTTPError error)
+        {
+            return this.httpErrors.Contains(error);
+        }
+    }
+}
