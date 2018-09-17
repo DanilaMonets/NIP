1c4bde31012040bfb73abad5b13222bdf1e3f1b5
Danil Gomodril <danil.monets@gmail.com>
2018-09-17 20:08:05 +0300

    Added XML comments
    Yo ;)

--- httpError/HTTPError.cs
+++ httpError/HTTPError.cs
@@ -9,6 +9,9 @@ using System.IO;
 
 namespace httpError
 {
+    /// <summary>
+    /// Represents class for HTTP error
+    /// </summary>
     public class HTTPError : IComparable
     {
         private int code;
@@ -16,6 +19,9 @@ namespace httpError
         private DateTime date;
         private static SortedList<int, string> listOfErrors;
 
+        /// <summary>
+        /// Static constructor for initialization static list of errors
+        /// </summary>
         static HTTPError()
         {
             listOfErrors = new SortedList<int, string>();
@@ -23,6 +29,9 @@ namespace httpError
             listOfErrors.Add(401, "Unauthorized");
             listOfErrors.Add(403, "Forbidden");
         }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:httpError.HTTPError"/> class
+        /// </summary>
         public HTTPError()
         {
             this.code = 0;
@@ -30,6 +39,11 @@ namespace httpError
             this.date = new DateTime();
 
         }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:httpError.HTTPError"/> class with specified code and date
+        /// </summary>
+        /// <param name="code"></param>
+        /// <param name="date"></param>
         public HTTPError(int code, DateTime date)
         {
             this.code = code;
@@ -40,6 +54,10 @@ namespace httpError
             this.date = date;
         }
 
+        /// <summary>
+        /// Gets variable code
+        /// </summary>
+        /// <value>The code</value>
         public int Code
         {
             get
@@ -51,6 +69,10 @@ namespace httpError
                 code = value;
             }
         }
+        /// <summary>
+        /// Gets variable description
+        /// </summary>
+        /// <value>The description</value>
         public string Description
         {
             get
@@ -62,6 +84,9 @@ namespace httpError
                 description = value;
             }
         }
+        /// <summary>
+        /// Gets and sets variable date
+        /// </summary>
         public DateTime Date
         {
             get
@@ -74,7 +99,11 @@ namespace httpError
             }
         }
 
-
+        /// <summary>
+        /// Gets description of error by its code
+        /// </summary>
+        /// <param name="key">Key (Error's code)</param>
+        /// <returns>String of description of error</returns>
         public static string getDescriptionOf(int key){
             string str;
             if (listOfErrors.TryGetValue(key, out str) == false)
@@ -83,12 +112,22 @@ namespace httpError
             }
             return str;
         }
-
+        /// <summary>
+        /// Determines whether the specified <see cref="object"/> is equal to the current <see cref="T:httpError.HTTPError"/>.
+        /// </summary>
+        /// <param name="obj">The <see cref="object"/> to compare with the current <see cref="T:httpError.HTTPError"/>.</param>
+        /// <returns><c>true</c> if the specified <see cref="object"/> is equal to the current
+        /// <see cref="T:httpError.HTTPError"/>; otherwise, <c>false</c>.</returns>
         public override bool Equals(object obj)
         {
 
             return code == (obj as HTTPError).Code && description.CompareTo((obj as HTTPError).description) == 0 ? true : false;
         }
+        /// <summary>
+        /// Compares instance to <see cref="object"/>
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns>1 0 -1</returns>
         public int CompareTo(object obj)
         {
             if (!(obj is HTTPError)) // perevirka ce is
@@ -129,10 +168,15 @@ namespace httpError
             }
             return result;
         }
+        /// <summary>
+        /// Returns a string that represents the current instance
+        /// </summary>
+        /// <returns>A <see cref="T:System.String"/></returns>
         public override string ToString()
         {
             return string.Format($"CODE: {this.code} DESCRIPTION: {this.description} DATE: {this.date}");
         }
+        // jepa
         public void Read(string s)
         {
             // string s = sr.ReadLine();
@@ -144,6 +188,10 @@ namespace httpError
             date = new DateTime(int.Parse(arrDate[0]), int.Parse(arrDate[1]), int.Parse(arrDate[2]));
 
         }
+        /// <summary>
+        /// Serves as a hash function for a <see cref="T:httpError.HTTPError"/> object 
+        /// </summary>
+        /// <returns>A hash code for this instance that is suitable for use in hashing algorithms and data structures such as a hash table</returns>
         public override int GetHashCode()
         {
             return this.ToString().GetHashCode();
