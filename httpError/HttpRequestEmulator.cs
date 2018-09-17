62e4525bbe06f7293b1750abe198c081b4758d87
Andeson25 <oleh.petryk@gmail.com>
2018-09-17 21:16:30 +0300

    Documentation for HttpRequestEmulation

--- httpError/HttpRequestEmulator.cs
+++ httpError/HttpRequestEmulator.cs
@@ -2,8 +2,15 @@ using System;
 
 namespace httpError
 {
+    /// <summary>
+    /// Class emulating Http Request 
+    /// </summary>
     public static class HttpRequestEmulator
     {
+        
+       /// <summary>
+       /// Make a fake request and get a random Http Error
+       /// </summary>
         public static void makeRequest()
         {
             var code = new Random().Next(400, 404);
