ed6f2e3d2943d9c02778e0175acc2b5b25c7ce38
Volodymyr Lysyshyn <lysyshynvova@gmail.com>
2018-09-17 14:03:32 +0100

    Added struct MyTextPair

---
+++ httpError/MyTextPair.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace httpError
+{
+    public struct MyTextPair
+    {
+        private string oldText;
+        private string newText;
+
+        public MyTextPair(string oldText, string newText)
+        {
+            this.oldText = oldText;
+            this.newText = newText;
+        }
+
+        public string OldText
+        {
+            get
+            {
+                return this.oldText;
+            }
+        }
+
+        public string NewText
+        {
+            get
+            {
+                return this.newText;
+            }
+        }
+    }
+}
