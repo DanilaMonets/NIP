0e1fb1ec7fb1c68ea8e7a947651b0d6c0b8165c4
Volodymyr Lysyshyn <lysyshynvova@gmail.com>
2018-09-17 23:34:54 +0100

    XML documentation for MyTextPair struct

--- httpError/MyTextPair.cs
+++ httpError/MyTextPair.cs
@@ -2,17 +2,29 @@
 
 namespace httpError
 {
+    /// <summary>
+    /// Represent a pair of two string values.
+    /// </summary>
     public struct MyTextPair
     {
         private string oldText;
         private string newText;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:httpError.MyTextPair"/> struct.
+        /// </summary>
+        /// <param name="oldText">Old text.</param>
+        /// <param name="newText">New text.</param>
         public MyTextPair(string oldText, string newText)
         {
             this.oldText = oldText;
             this.newText = newText;
         }
 
+        /// <summary>
+        /// Gets the old text.
+        /// </summary>
+        /// <value>The old text.</value>
         public string OldText
         {
             get
@@ -21,6 +33,10 @@ namespace httpError
             }
         }
 
+        /// <summary>
+        /// Gets the new text.
+        /// </summary>
+        /// <value>The new text.</value>
         public string NewText
         {
             get
