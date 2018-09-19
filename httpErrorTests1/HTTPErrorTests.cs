96495c77b4f4944f3f92a22dc68302aeb6eddd11
Danil Gomodril <danil.monets@gmail.com>
2018-09-19 19:53:36 +0300

    Added HttpError.Tests for unit-testing HttpError class

---
+++ httpErrorTests1/HTTPErrorTests.cs
@@ -0,0 +1,25 @@
+﻿using HttpError;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpError.Tests
+{
+    [TestClass()]
+    public class HTTPErrorTests
+    {
+        [TestMethod()]
+        public void HTTPErrorTest()
+        {
+            var kek = DateTime.Now;
+            HTTPError hTTPError = new HTTPError(404, kek);
+
+            Assert.AreEqual(kek, hTTPError.Date);
+            Assert.AreEqual(404, hTTPError.Code);
+        }
+    }
+
+}
